4b71c94e31e5cccee5b9a363ce7227761681a654
Abubakar-88 <76661821+Abubakar-88@users.noreply.github.com>
2023-06-17 16:52:10 +0600

    Update Hooks.java

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,7 +1,5 @@
 package stepDefinitions;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import java.net.MalformedURLException;
